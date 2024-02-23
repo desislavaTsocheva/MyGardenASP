b15cf6d24dac72d9a286e7e3ef007fdf56d55781
pramataroff <milenpramatarov@gmail.com>
2024-02-23 10:33:25 +0200

    IdentityRoles

--- MyGardenWEB/MyGardenWEB/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MyGardenWEB/MyGardenWEB/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -76,28 +76,28 @@ namespace MyGardenWEB.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             /// 
-            [Required (ErrorMessage ="The field is requared!")]
-            [Display(Name = "User Name")]
+            [Required(ErrorMessage ="The field is requared!")]
+            [Display(Name = "UserName")]
             public string UserName { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "The field is requared!")]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
-
-            [Required(ErrorMessage = "The field is requared!")]
-            [Display(Name = "First Name")]
-            public string FirstName { get; set; }
-
-            [Required(ErrorMessage = "The field is requared!")]
-            [Display(Name = "Last Name")]
-            public string LastName { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
+            /// 
+            [Required(ErrorMessage = "The field is requared!")]
+            [Display(Name = "FirstName")]
+            public string FirstName { get; set; }
             [Required]
+            [Display(Name = "LastName")]
+            public string LastName { get; set; }
+
+            [Required(ErrorMessage = "The field is requared!")]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
@@ -126,28 +126,22 @@ namespace MyGardenWEB.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                //var user = CreateUser();
-
                 Client user = new Client
                 {
                     UserName = Input.UserName,
-                    Email = Input.Email,
+                    Email=Input.Email,
                     FirstName=Input.FirstName,
-                    LastName=Input.LastName,    
+                    LastName=Input.LastName,
                 };
-
                 await _userStore.SetUserNameAsync(user, Input.UserName, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-                
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
-                    //Добавяне на роля user 
                     await _userManager.AddToRoleAsync(user, "User");
-
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
