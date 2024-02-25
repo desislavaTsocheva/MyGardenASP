fd33afe8c78c6ddd4a30daa95b3befb3ae8a76b5
DesislavaTsocheva <desislava_tsocheva@abv.bg>
2024-02-25 11:13:43 +0200

    design

--- MyGardenWEB/MyGardenWEB/wwwroot/js/site.js
+++ MyGardenWEB/MyGardenWEB/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//<script src="http://smtpjs.com/v3/smtp.js"></script>
+//function SendMail() {
+//    Email.send(
+//        {
+//            Host: "smtp.gmail.com",
+//            UserName: "tsochevadesislava@gmail.com",
+//            Password: "password",
+//            To: "tsochevadesislava@gmail.com",
+//            From: document.getElementById("email").value,
+//            Subject: "SOMETHING"
+//            Body: "jdjshcdj"
+//        }
+//    ).then(message => alert(message));
+//}
