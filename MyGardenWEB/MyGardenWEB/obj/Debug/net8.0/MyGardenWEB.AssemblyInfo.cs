c1b80bbb16123f687484a13a1a73f28489e93039
DesislavaTsocheva <desislava_tsocheva@abv.bg>
2024-03-27 14:32:51 +0200

    responsive design

--- MyGardenWEB/MyGardenWEB/obj/Debug/net8.0/MyGardenWEB.AssemblyInfo.cs
+++ MyGardenWEB/MyGardenWEB/obj/Debug/net8.0/MyGardenWEB.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyGardenWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3d28ee12815c26fbb6dafa8e9178bbfefe11933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9a01d2e9d8b0e3a1fd860771e86a395c06a25d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyGardenWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyGardenWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
